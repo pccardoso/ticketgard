ad2afbf4c236c6098dfa66348dd0fdb15ee43141
Paulo Cesar <paulo.cardoso2408@gmail.com>
2025-05-02 10:45:21 -0300

    Alteração do ícone, modo dark

--- resources/js/Pages/Login/Login.vue
+++ resources/js/Pages/Login/Login.vue
@@ -5,9 +5,9 @@
         <link rel="icon" type="image/svg+xml" href="../../../img/icon.svg" />
     </Head>
 
-    <div class=" flex items-center justify-center min-h-screen bg-gray-100">
-            <div class="">
+    <div class="flex items-center justify-center min-h-screen bg-gray-100">
 
+            <div class="">
                 <div class="mt-20 w-full p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8  ">
                     <form class="space-y-6" action="#" @submit.prevent="login()">
 
@@ -32,7 +32,7 @@
 
                         <ButtonPrimary type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center   dark:focus:ring-blue-800">Entrar</ButtonPrimary>
                     
-                        <p class="text-gray-400 text-center m-1">v.1.1.8</p>
+                        <p class="text-gray-400 text-center m-1">v.1.1.9</p>
                     </form>
                 </div>
             </div>
