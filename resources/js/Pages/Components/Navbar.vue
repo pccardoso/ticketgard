cfd3fd8f133c581f5124f79875fd2815992a7f38
Paulo Cesar <paulo.cardoso2408@gmail.com>
2025-05-02 09:14:24 -0300

    Remoção do ícone de notificação

--- resources/js/Pages/Components/Navbar.vue
+++ resources/js/Pages/Components/Navbar.vue
@@ -201,56 +201,6 @@
             <div class="inline-flex mb-2 text-white bg-blue-600 rounded-2xl p-1 pl-3 pr-3 shadow-lg"><IconUser class="text-color-500"/>{{ user.name }}</div>
             
             <div v-if="user.vip" class="inline-flex ml-2 mb-2 text-white bg-yellow-400 rounded-2xl p-1 pl-3 pr-3 shadow-lg" title="Seu usuário é VIP"><img src="../../../img/vip.png.png" class="w-5 mr-1">Usuário VIP</div>
-            
-            <div class="">
-
-               <svg @click="actionShowNotification()" class="w-8 h-8" :class="showNotification ? 'text-blue-600':'text-blue-400'" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M17.133 12.632v-1.8a5.407 5.407 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V3.1a1 1 0 0 0-2 0v2.364a.933.933 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C6.867 15.018 5 15.614 5 16.807 5 17.4 5 18 5.538 18h12.924C19 18 19 17.4 19 16.807c0-1.193-1.867-1.789-1.867-4.175Zm-13.267-.8a1 1 0 0 1-1-1 9.424 9.424 0 0 1 2.517-6.391A1.001 1.001 0 1 1 6.854 5.8a7.43 7.43 0 0 0-1.988 5.037 1 1 0 0 1-1 .995Zm16.268 0a1 1 0 0 1-1-1A7.431 7.431 0 0 0 17.146 5.8a1 1 0 0 1 1.471-1.354 9.424 9.424 0 0 1 2.517 6.391 1 1 0 0 1-1 .995ZM8.823 19a3.453 3.453 0 0 0 6.354 0H8.823Z"/>
-               </svg>
-
-               <!--
-               <div v-if="showNotification" class="bg-blue-100 border-2 border-blue-400 rounded-lg absolute mt-2 p-2 shadow-sm w-80">
-                  
-                  <div class="h-50 overflow-auto space-x-5">
-
-                     <div class="bg-blue-400 text-white p-1 rounded-sm m-1">
-                        Nova Mensagem: Leo Junior
-                     </div>
-
-                     <div class="bg-blue-400 text-white p-1 rounded-sm m-1">
-                        Nova Mensagem: Leo Junior
-                     </div>
-
-                     <div class="bg-blue-400 text-white p-1 rounded-sm m-1">
-                        Nova Mensagem: Leo Junior
-                     </div>
-
-                     <div class="bg-blue-400 text-white p-1 rounded-sm m-1">
-                        Nova Mensagem: Leo Junior
-                     </div>
-
-                     <div class="bg-red-400 text-white p-1 rounded-sm m-1">
-                        Nova Mensagem: Leo Junior
-                     </div>
-                     
-                     <div class="bg-red-400 text-white p-1 rounded-sm m-1">
-                        Nova Mensagem: Leo Junior
-                     </div>
-
-                     <div class="bg-red-400 text-white p-1 rounded-sm m-1">
-                        Nova Mensagem: Leo Junior
-                     </div>
-
-                     <div class="bg-blue-400 text-white p-1 rounded-sm m-1">
-                        Nova Mensagem: Leo Junior
-                     </div>
-
-                  </div>
-                  
-               </div>
-               -->
-
-            </div>
 
          </div>
 
@@ -347,16 +297,18 @@
                
                if(result.data.lista.length){
 
+                  //recuperando o title da página atual
                   this.titleTemp = document.title;
 
+                  //definindo o novo title da página atual
                   document.title = `${result.data.lista.length} novo(s) ticket(s)`;
 
+                  //ativando o alert de notificações
                   this.actionShowNotification();
-                  this.listTicketNotification = result.data.lista;
-                  console.log(result.data.lista);
 
-               }else{
-                  console.log("Sem chamados");
+                  //carregando a lista de usuários da notificação encontrada
+                  this.listTicketNotification = result.data.lista;
+
                }
 
             })
