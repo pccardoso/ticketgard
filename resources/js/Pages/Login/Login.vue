7581f4aaf34e7417d9218135e97012ac099765dc
Paulo Cesar <paulo.cardoso2408@gmail.com>
2025-06-16 09:39:19 -0300

    Atualizações

--- resources/js/Pages/Login/Login.vue
+++ resources/js/Pages/Login/Login.vue
@@ -32,7 +32,7 @@
 
                         <ButtonPrimary type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center   dark:focus:ring-blue-800">Entrar</ButtonPrimary>
                     
-                        <p class="text-gray-400 text-center m-1">v.2.0.1</p>
+                        <p class="text-gray-400 text-center m-1">v.2.0.2</p>
                     </form>
                 </div>
             </div>
